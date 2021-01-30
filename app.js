6f834450b675e9a087acc15c97f872d8a589663b
lucky0011198 <68720142+lucky0011198@users.noreply.github.com>
2021-01-30 11:35:16 +0530

    cancel

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const path =require ("path");
 const app =express();
 require("./db/conn");
 //const static_path= path.join(__dirname,"../public")
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 4000;
 app.set('view engine', '.hbs');
 app.get('/',(req,res)=>{
-    res.render("index");
+    res.render("login");
+})
+app.get('/login',(req,res)=>{
+    res.render("login");
 })
 app.listen(port,()=>{
-    console.log("express is connected at 3000")
+    console.log("express is connected at 4000")
 
 })
